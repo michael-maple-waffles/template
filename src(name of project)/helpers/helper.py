3d5b82f314cc84deb7cd822de05a84c971646719
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-02-17 10:52:08 -0700

    first commit

---
+++ src(name of project)/helpers/helper.py
@@ -0,0 +1,3 @@
+#contains base helper functions that will be used throughout this code.
+
+#!!!DO NOT!!! import any of your code into these files!!!
---
+++ src(name of project)/main.py
@@ -0,0 +1,5 @@
+#this is required
+
+#project runs from main, things are imported, set up main loop, contains almost no code. main loopfor running project
+
+#simple user interface, or if complex make it a function on a seperate file.
